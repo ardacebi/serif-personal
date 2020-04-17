f40fc82c83a967cee6ae625e8baf4053b414733c
Arda Çebi <ardacebi1@gmail.com>
2020-04-17 20:26:44 +0300

    Initial

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+
+<html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<head>
+  <meta charset="utf-8">
+  <title>ARDA ÇEBİ</title>
+  <link rel='stylesheet' href='style.css' />
+  <link href="https://fonts.googleapis.com/css?family=Vidaloka&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700,800,900&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/86f7f6662f.js" crossorigin="anonymous"></script>
+  <script type="text/javascript">
+    (function () {
+      var css = document.createElement('link');
+      css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css';
+      css.rel = 'stylesheet';
+      css.type = 'text/css';
+      document.getElementsByTagName('head')[0].appendChild(css);
+    })();
+  </script>
+
+</head>
+
+<body>
+
+  <div class="header">
+    <a href="#default" class="logo">Arda Çebi</a>
+    <div class="header-right">
+      <a class="active" href="#home">Home</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
+  </div>
+
+  <main id="swup" class="transition-fade">
+
+    <section class="intro">
+      <div class="content">
+        <div class="scroll-indicator" id="section01" data-scroll-indicator-title="Hello World"></div>
+        <h2 class="landingdescription">Developer, entrepreneur, photographer, designer and space/science enthusiast.
+        </h2>
+      </div>
+    </section>
+
+    <script>
+      var sidebar = new StickySidebar('#sidebar', { topSpacing: 20 });
+    </script>
+
+
+    <div id="main-content" class="main">
+      <div id="sidebar" class="sidebar">
+
+</template>
+
+<style>
+
+</style>
---
+++ src/app.js
@@ -0,0 +1,6 @@
+new Vue({
+	  router,
+	  render: h => h(App)
+
+}).$mount('#app');
+
